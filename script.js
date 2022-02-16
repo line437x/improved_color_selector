16e8799132a14fd96de3d829f841d4cc7859686a
lineberner <line437x@stud.kea.dk>
2022-02-16 15:00:52 +0100

    improved version done

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
 "use strict";
 
-let userValue = document.querySelector("#color_value").value;
+let userValue;
 let input = document.querySelector("#color_value");
-let rgb;
 
-window.addEventListener("load", start);
-input.addEventListener("input", showColor);
+window.addEventListener("load", setup);
+input.addEventListener("input", getColor);
 
-function start() {
-  showColor();
+function setup() {
+  getColor();
 }
-
-function showColor() {
+// --------------- Getting a selected color from the user ---------------
+function getColor() {
   userValue = document.querySelector("#color_value").value;
-  displayColor();
-  hexToRGB(userValue);
-  renderHexValue();
+  let rgbValue = hexToRGB(userValue);
+  let hslValue = rgbToHsl(rgbValue);
+  let hexValue = rgbToHex(rgbValue);
+  let cssValue = rgbToCss(rgbValue);
+
+  showColorValues(rgbValue, hslValue, hexValue, cssValue);
 }
 
-//convert input value to RGB
+// --------------- Showing a selected color ---------------
+function showColorValues(rgbV, hslV, hexV, cssV) {
+  displayHexValue(hexV);
+  displayRgbValue(rgbV);
+  displayHslValue(hslV);
+  displayCssValue(cssV);
+  displayColorBox();
+}
+
+// --------------- Converting RGB to CSS usable string, like rgb(100, 123, 192) ---------------
+function rgbToCss(rgb) {
+  let css = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+  console.log(css);
+  return css;
+}
+// --------------- Converting RGB to hex ---------------
+function rgbToHex(rgb) {
+  let r = rgb.r;
+  let g = rgb.g;
+  let b = rgb.b;
+  let hex = ("0" + r.toString(16)).slice(-2) + ("0" + g.toString(16)).slice(-2) + ("0" + b.toString(16)).slice(-2);
+
+  return hex;
+}
+
+// --------------- Converting hex to RGB ---------------
 function hexToRGB(hex) {
   let r = parseInt(hex.substring(1, 3), 16);
   let g = parseInt(hex.substring(3, 5), 16);
   let b = parseInt(hex.substring(5, 7), 16);
 
-  rgb = `${r}, ${g}, ${b}`;
-  displayRgbValue();
-  rgbToHsl(r, g, b);
+  let rgb = { r, g, b };
+  return rgb;
 }
 
-//convert input value to HSL
-function rgbToHsl(r, g, b) {
+// --------------- Converting RGB to HSL ---------------
+function rgbToHsl(rgb) {
+  let r = rgb.r;
+  let g = rgb.g;
+  let b = rgb.b;
+
   r /= 255;
   g /= 255;
   b /= 255;
@@ -66,25 +96,26 @@ function rgbToHsl(r, g, b) {
   l *= 100;
 
   let hsl = { h: Math.floor(h), s: Math.floor(s), l: Math.floor(l) };
-  displayHslValue(hsl);
+  return hsl;
 }
 
-//display color
-function displayColor() {
+// --------------- Showing the color as a colored box in CSS ---------------
+function displayColorBox() {
   document.querySelector(".color_box").style.backgroundColor = userValue;
 }
-
-//udskrive input værdien i hex felt
-function renderHexValue() {
+// --------------- Showing the color as hex ---------------
+function displayHexValue() {
   document.querySelector(".hex_code").textContent = userValue;
 }
-
-// display rgb value funktion
-function displayRgbValue() {
-  document.querySelector(".rgb_code").textContent = rgb;
+// --------------- Showing the color as RGB ---------------
+function displayRgbValue(rgb) {
+  document.querySelector(".rgb_code").textContent = `${rgb.r}, ${rgb.g}, ${rgb.b}`;
 }
-
-// display hsl value funktion
+// --------------- Showing the color as HSL ---------------
 function displayHslValue(hsl) {
-  document.querySelector(".hsl_code").textContent = `${hsl.h}, ${hsl.s}%, ${hsl.l}%`;
+  document.querySelector(".hsl_code").textContent = `${hsl.h}°, ${hsl.s}%, ${hsl.l}%`;
+}
+// --------------- Showing the color as CSS ---------------
+function displayCssValue(css) {
+  document.querySelector(".css_code").textContent = css;
 }
